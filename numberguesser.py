1eaecd55dd4b7e24303020b87253bacfa1fd4a5c
ajw109 <136182723+ajw109@users.noreply.github.com>
2023-10-17 15:59:01 -0400

    Update numberguesser.py

--- numberguesser.py
+++ numberguesser.py
@@ -16,14 +16,18 @@ while True:
 	high_num = int(input(f"\nEnter higher range: "))
 	random_num = random.randint(1,high_num)
 	attempts = 0
+	guess = None
 
 	print(f"\nI'm thinking of a number from 1 to", high_num,".....\n")
 
-	while attempts < 10 or guess == random_num:
+	while guess != random_num:
 		#Feedback after every round. 
 		guess = int(input("Your guess: "))
 		attempts = attempts + 1 
-		if guess < 1 or guess > high_num:
+		if attempts > 9:
+			print("Too many tries. You lost.")
+			break
+		elif guess < 1 or guess > high_num:
 			print("Invalid input. Please enter a number to try again.")
 		elif guess < random_num:
 			print("Too low. Try again.")
@@ -37,11 +41,10 @@ while True:
 			break
 			#If guess is correct, break the loop.
 		    
-		if attempts >= 10:
-			print("Too many tries. You lost.")
+		
 
 	again = input(f"\nWould you like to play again? (y/n): ")
-	if again.lower() != "y":
+	if again != "y":
 		print(f"\nBye!\n")
 		break
-		#If player does not want to play again, break the loop and end the game/program. 
+		#If player does not want to play again, break the loop and end.
